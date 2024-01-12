75b2f697711469b8e80c064d0d8431e4db1aeb8d
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-01-12 09:10:04 +0900

    [level Lv.0] Title: 몫 구하기, Time: 0.24 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120805. 몫 구하기/몫 구하기.cs
+++ 프로그래머스/Lv.0/120805. 몫 구하기/몫 구하기.cs
@@ -2,11 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int num1, int num2) {
-        int answer = 0;
-        if(num1> 0 && num1<= 100 && num2 >0 && num2<= 100)
-        {
-            answer = num1/num2;
-        }
+        int answer = num1 / num2
         return answer;
     }
 }
