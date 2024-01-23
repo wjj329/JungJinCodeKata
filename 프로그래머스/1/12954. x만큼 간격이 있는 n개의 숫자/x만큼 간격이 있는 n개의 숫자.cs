5e62516d3d74e67c9f4fbb11019058f2f405a8f3
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-01-23 09:47:10 +0900

    [level 1] Title: x만큼 간격이 있는 n개의 숫자, Time: 0.29 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        long num = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            num += x;
+            answer[i] = num;
+        }
+        return answer;
+    }
+}
