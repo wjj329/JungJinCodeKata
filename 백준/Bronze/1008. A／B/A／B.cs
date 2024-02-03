cf627a5152ff58ea1b0ec6c8bc053a7f0c2d37d9
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-02-03 16:15:07 +0900

    [Bronze V] Title: A/B, Time: 92 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Threading.Channels;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine(double.Parse(s[0]) / double.Parse(s[1]));
+        }    }
+}
