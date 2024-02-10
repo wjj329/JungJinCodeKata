aa109f79efe9880b275bf70db67447d3fe3d49cf
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-02-11 00:35:21 +0900

    [Bronze V] Title: A+B, Time: 60 ms, Memory: 5500 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cs
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -1,15 +1,3 @@
-using System;
 
-namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
             string[] s = Console.ReadLine().Split();
-
-
             Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
-        }
-    }
-}
