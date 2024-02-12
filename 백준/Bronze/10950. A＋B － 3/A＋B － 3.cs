130a3826c88c03be1fcb0da32c14b8dbe29d4c80
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-02-12 18:04:02 +0900

    [Bronze V] Title: A+B - 3, Time: 72 ms, Memory: 5620 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cs
@@ -0,0 +1,10 @@
+            int a = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < a; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                int num1 = int.Parse(s[0]);
+                int num2 = int.Parse(s[1]);
+
+                Console.WriteLine(num1+num2 );
+            }
