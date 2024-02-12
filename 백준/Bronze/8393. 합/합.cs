beb254f0bbb01f2068aaf09610b7ebe07e62222d
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-02-12 18:13:03 +0900

    [Bronze V] Title: 합, Time: 64 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cs
@@ -0,0 +1,8 @@
+            int sum = 0;
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= n; i++)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
