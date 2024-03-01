f989dea1393cdde8de8258be68f1056fe02d5438
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-03-02 02:25:39 +0900

    [Bronze V] Title: A+B - 5, Time: 68 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cs
@@ -0,0 +1,10 @@
+            while (true)
+            {
+                string[] s = Console.ReadLine().Split();
+                int a = int.Parse(s[0]);
+                int b = int.Parse(s[1]);
+
+                if (a == 0 && b == 0) break;
+
+                Console.WriteLine(a+b);
+            }
