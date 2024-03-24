2bd678e291b909a2315e46fe82c17fbc009b630f
wjj329 <154484828+wjj329@users.noreply.github.com>
2024-03-25 03:39:33 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.92 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(int[] food) {
+        string answer = "";
+        string str = "";
+        
+        for(int i = 1; i < food.Length; i++)
+        {
+            for(int j = 0; j < food[i] / 2; j++)
+            {
+                answer += i.ToString();
+            }
+        }
+        str = String.Concat(answer.Reverse());
+        answer += "0" + str;
+        return answer;
+    }
+}
